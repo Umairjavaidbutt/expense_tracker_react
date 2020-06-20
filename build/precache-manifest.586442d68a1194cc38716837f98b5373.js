6118c8496152bda669d637d7c9183ceb0dc126e7
unknown <umair.javaid.butt@gmail.com>
2020-06-20 21:36:17 +0500

    base expense tracker

---
+++ build/precache-manifest.586442d68a1194cc38716837f98b5373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ac10949b06f5a6f384b6dcb935dff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4adadc73000962e87cbd",
+    "url": "/static/css/main.f65cf5c3.chunk.css"
+  },
+  {
+    "revision": "eaad660e0c1a05d1af4e",
+    "url": "/static/js/2.6b278f5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b278f5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4adadc73000962e87cbd",
+    "url": "/static/js/main.0dd53226.chunk.js"
+  },
+  {
+    "revision": "27b00dc97d8be027a4ee",
+    "url": "/static/js/runtime-main.745e75de.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { AccountSummary } from './components/AccountSummary';
+import { TransactionHistory } from './components/TransactionHistory';
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+
+
+function App() {
+  return (
+    <GlobalProvider>
+    <div>
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <AccountSummary/>
+        <TransactionHistory/>
+        <AddTransaction/>
+      </div>
+    </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
